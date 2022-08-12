7841a5fe2aad36815c4138cdf304460372fd65b5
muhammadabdullahi190A-u-usman <muhammadabdullahi190@gmail.com>
2022-08-12 17:14:14 +0100

    project_task_one

---
+++ project_one/task1_email.go
@@ -0,0 +1,8 @@
+package main
+// program that shows your email adress & name
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Muhammad Abdullahi Usman")
+	fmt.Println("your email address is: muhammadabdullahi190@gmail.com")
+}
---
+++ project_one/task1_stack_track.go
@@ -0,0 +1,8 @@
+package main
+
+// program that shows your email adress & name
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, my Stack track is BACKEND!")
+}
